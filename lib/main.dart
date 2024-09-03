d1db8b460dc5a5a66c246f4ca0ae1434cd6974ba
Mirzaazmath <azmath9603399270@gmail.com>
2024-09-03 17:52:21 +0530

    added new output

--- lib/main.dart
+++ lib/main.dart
@@ -66,115 +66,126 @@ class _HomeScreenState extends State<HomeScreen> {
           alignment: Alignment.center,
           children: [
             AnimatedPositioned(
-              right: rollCan ? -150: MediaQuery.sizeOf(context).width / 3,
+              right: rollCan ? -180: MediaQuery.sizeOf(context).width / 3,
               duration: const Duration(seconds: 1),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 6,
-                    width: 150,
-                    decoration: BoxDecoration(
-                      color: Colors.grey.shade500,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                  ),
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(8),
-                    child: Container(
-                      height: 400,
+              child: Container(
+                decoration:const  BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black54,
+                    blurRadius: 30,
+                    offset: Offset(-20,10)
+                  )
+                ]
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 6,
                       width: 150,
-                      foregroundDecoration: const BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: [
-                            Colors.black54,
-                            Colors.transparent,
-                          ],
-                        ),
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade500,
+                        borderRadius: BorderRadius.circular(20),
                       ),
-                      decoration: const BoxDecoration(color: Colors.black),
-                      child: Stack(
-                        alignment: Alignment.center,
-                        children:[
-                          PageView.builder(
-                          controller: _pageController,
-                          itemCount: images.length,
-                          itemBuilder: (context, index) {
-                            return Container(
-                              decoration: BoxDecoration(
-                                image: DecorationImage(
-                                  image: AssetImage(images[index]),
-                                  fit: BoxFit.fill,
+                    ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Container(
+                        height: 400,
+                        width: 150,
+                        foregroundDecoration: const BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Colors.black54,
+                              Colors.transparent,
+                            ],
+                          ),
+                        ),
+                        decoration: const BoxDecoration(color: Colors.black),
+                        child: Stack(
+                          alignment: Alignment.center,
+                          children:[
+                            PageView.builder(
+                            controller: _pageController,
+                            itemCount: images.length,
+                            itemBuilder: (context, index) {
+                              return Container(
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    image: AssetImage(images[index]),
+                                    fit: BoxFit.fill,
+                                  ),
                                 ),
+                              );
+                            },
+                          ),
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: Container(
+                                margin:const  EdgeInsets.only(left: 10),
+                                width: 5,
+                                height: double.infinity,
+                              decoration:const  BoxDecoration(
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.white,
+                                    blurRadius: 5
+                                  )
+                                ]
                               ),
-                            );
-                          },
+
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.topRight,
+                              child: Container(
+                                margin:const  EdgeInsets.only(right: 10),
+                                width: 5,
+                                height: double.infinity,
+                                decoration:const  BoxDecoration(
+                                    boxShadow: [
+                                      BoxShadow(
+                                          color: Colors.white,
+                                          blurRadius: 10
+                                      )
+                                    ]
+                                ),
+
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.center,
+                              child: Container(
+                                margin:const  EdgeInsets.only(right: 10),
+                                width: 10,
+                                height: double.infinity,
+                                decoration:const  BoxDecoration(
+                                    boxShadow: [
+                                      BoxShadow(
+                                          color: Colors.white,
+                                          blurRadius: 30
+                                      )
+                                    ]
+                                ),
+
+                              ),
+                            ),
+                            ]
                         ),
-                          Align(
-                            alignment: Alignment.topLeft,
-                            child: Container(
-                              margin:const  EdgeInsets.only(left: 10),
-                              width: 5,
-                              height: double.infinity,
-                            decoration:const  BoxDecoration(
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.white,
-                                  blurRadius: 5
-                                )
-                              ]
-                            ),
-
-                            ),
-                          ),
-                          Align(
-                            alignment: Alignment.topRight,
-                            child: Container(
-                              margin:const  EdgeInsets.only(right: 10),
-                              width: 5,
-                              height: double.infinity,
-                              decoration:const  BoxDecoration(
-                                  boxShadow: [
-                                    BoxShadow(
-                                        color: Colors.white,
-                                        blurRadius: 10
-                                    )
-                                  ]
-                              ),
-
-                            ),
-                          ),
-                          Align(
-                            alignment: Alignment.center,
-                            child: Container(
-                              margin:const  EdgeInsets.only(right: 10),
-                              width: 10,
-                              height: double.infinity,
-                              decoration:const  BoxDecoration(
-                                  boxShadow: [
-                                    BoxShadow(
-                                        color: Colors.white,
-                                        blurRadius: 30
-                                    )
-                                  ]
-                              ),
-
-                            ),
-                          ),
-                          ]
                       ),
                     ),
-                  ),
-                  Container(
-                    height: 6,
-                    width: 150,
-                    decoration: BoxDecoration(
-                      color: Colors.grey.shade500,
-                      borderRadius: BorderRadius.circular(10),
+                    Container(
+                      height: 6,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade500,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ],
